b86dbd30ceda7ef88cc44c98991a9a5f8d7a5517
Jairo Emmanuel Martinez Olvera <jairoemmanuelmartinez@gmail.com>
2023-05-24 21:20:11 -0600

    add loginPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:contador/pages/home_page.dart';
+import 'package:contador/pages/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -9,8 +9,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: HomePage(),
+      home: LoginPage(),
     );
   }
 }
